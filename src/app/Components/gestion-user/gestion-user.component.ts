117611f93d430a6a52f450f9f6f5f3e3274cc274
natalia mogollón <110124286+nathis06@users.noreply.github.com>
2023-05-01 13:34:08 -0500

    Update gestion-user.component.ts

--- src/app/Components/gestion-user/gestion-user.component.ts
+++ src/app/Components/gestion-user/gestion-user.component.ts
@@ -1,28 +1,45 @@
 import { Component , ViewChild} from '@angular/core';
 import { MatTable } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { Prestamo } from 'src/app/Model/Prestamo';
+import { PrestamoService } from 'src/app/service/prestamo.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-gestion-user',
   templateUrl: './gestion-user.component.html',
   styleUrls: ['./gestion-user.component.css']
 })
 export class GestionUserComponent {
-  columnas: string[] = ['ID', 'Fecha', 'Hora','Nom_Escenario','Acciones'];
-
-  datos: Articulo[] = [new Articulo(1, '22/04/23', '8:00 am',"Fut sala1"),
-
-];
-
-articuloselect: Articulo = new Articulo(0, "", "","");
-
-@ViewChild(MatTable) tabla1!: MatTable<Articulo>;
-
-
-
+  booking: Prestamo[];
+constructor(private ServicePrestamo:PrestamoService, private router:Router){ }
+ngOnInit(): void {
+  this.getBooking(); 
 }
 
-
-export class Articulo {
-constructor(public ID: number, public Fecha: string, public Hora: string, public Nom_Escenario: string) {
+private getBooking(){
+  this.ServicePrestamo.readAllBooking().subscribe(dato=>{
+    this.booking=dato;
+  });
 }
+deleteB(id: number) {
+  this.ServicePrestamo.deleteB(id).subscribe(
+    dato => {
+      console.log(dato);
+      this.getBooking();
+    },
+
+   error => {
+      console.log(error);
+      // si el error es una cadena, significa que es el mensaje personalizado que creamos en el método deleteS()
+      if (typeof error === 'string') {
+      
+        Swal.fire('No se  Elimino', `El Escenario no ha sido eliminado`, 'error');
+      }
+    }
+  );
+ Swal.fire('Se Elimino', `El Escenario ha sido eliminado con éxito`, 'success');
+
+  }
+
 
 }
