a0b8864f2ec739fb9efd5755abddb9a6c614d284
natalia mogollón <110124286+nathis06@users.noreply.github.com>
2023-05-01 13:24:07 -0500

    Update user.service.ts

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,10 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from '../Model/Usuario';
+import { Subject } from 'rxjs';
+import { CookieService } from "ngx-cookie-service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  public loginStatusSubjec = new Subject<boolean>();
 
-constructor() { }
+  private Url='http://localhost:8080/api/Users/user/login'
 
+  constructor(private http:HttpClient, private cookies: CookieService) { }
+
+  login( user: any): Observable<any> {
+    return this.http.post(`${this.Url}`, user);
+  }
+   //obtener por id
+   read(id:number):Observable<Usuario>{
+    return this.http.get<Usuario>(`${this.Url}/${id}`);
+  }
+
+  setToken(token: string) {
+    this.cookies.set("token", token);
+  }
+  getToken() {
+    return this.cookies.get("token");
+  }
+  getUser() {
+    return this.http.get("https://reqres.in/api/Users/user");
+  }
+  getUserLogged() {
+    const token = this.getToken();
+    // Aquí iría el endpoint para devolver el usuario para un token
+  }
 }
