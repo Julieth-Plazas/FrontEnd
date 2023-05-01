3d3a367db104efbdaf1d2adb698896a46fb21518
natalia mogollón <110124286+nathis06@users.noreply.github.com>
2023-05-01 13:42:24 -0500

    Update visualizar-admon.component.ts

--- src/app/Components/visualizar-admon/visualizar-admon.component.ts
+++ src/app/Components/visualizar-admon/visualizar-admon.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit, ViewChild} from '@angular/core';
 import { MatTable } from '@angular/material/table';
 import { da } from 'date-fns/locale';
 import { Escenario } from 'src/app/Model/Escenario';
-import { ListarEscService } from 'src/app/service/listar-esc.service';
+import { ListarEscService} from 'src/app/service/listar-esc.service';
 import { Router } from '@angular/router';
+import{ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-visualizar-admon',
@@ -13,7 +16,9 @@ import { Router } from '@angular/router';
 
 export class VisualizarAdmonComponent implements OnInit{
   Scenary: Escenario[];
-  constructor(private ServiceScenary:ListarEscService, private router:Router){  }
+  errorMessage: string;
+
+  constructor(private ServiceScenary:ListarEscService, private router:Router,  private toastr: ToastrService){  }
 
   ngOnInit(): void {
     this.getScenary(); 
@@ -24,14 +29,28 @@ export class VisualizarAdmonComponent implements OnInit{
   }
 
 
-deleteS(id:number){
-    this.ServiceScenary.deleteS(id).subscribe(dato=>{
-    console.log(dato);
-     this.getScenary();
-    });
+deleteS(id: number) {
+  this.ServiceScenary.deleteS(id).subscribe(
+    dato => {
+      console.log(dato);
+      this.getScenary();
+    },
+
+   error => {
+      console.log(error);
+      // si el error es una cadena, significa que es el mensaje personalizado que creamos en el método deleteS()
+      if (typeof error === 'string') {
+      
+        Swal.fire('No se  Elimino', `El Escenario no ha sido eliminado`, 'error');
+      }
+    }
+  );
+ Swal.fire('Se Elimino', `El Escenario ha sido eliminado con éxito`, 'success');
+
   }
 
-  private getScenary(){
+  
+ private getScenary(){
     this.ServiceScenary.readAllScenarys().subscribe(dato=>{
       this.Scenary=dato;
     });
