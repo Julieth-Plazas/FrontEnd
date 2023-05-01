65e1925e0f845e32aab2d4c87a5ab83e344423d3
natalia mogollón <110124286+nathis06@users.noreply.github.com>
2023-05-01 13:35:51 -0500

    Update home.component.ts

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit{
+  constructor(public userService: UserService) {}
+  ngOnInit() {
+    this.getUserLogged();
+  }
+  getUserLogged() {
+    this.userService.getUser().subscribe((user) => {
+      console.log(user);
+    });
+  }
 }
+
